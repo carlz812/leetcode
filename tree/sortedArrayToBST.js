3ef7e6aec537646270ccf8dbb2cf2ba18fda976b
carlzz <carlzzzz@163.com>
2018-08-20 14:20:36 +0800

    update

--- tree/sortedArrayToBST.js
+++ tree/sortedArrayToBST.js
@@ -9,20 +9,18 @@
  * @param {number[]} nums
  * @return {TreeNode}
  */
-var sortedArrayToBST = function (nums) {
-    var len = nums.length;
-    var root = null;
-    root = recursion(nums, 1, len, root)
-    nums = null;
-    return root;
-};
 
-function recursion(array, lhs, rhs, root) {
-    if (lhs <= rhs) {
-        var mid = (lhs + rhs + 1) >> 1;
-        root = new TreeNode(array[mid - 1]);
-        root.left = recursion(array, lhs, mid - 1, root.left);
-        root.right = recursion(array, mid + 1, rhs, root.right);
+var sortedArrayToBST = function (nums) {
+    return dichotomy(nums, 0, nums.length - 1);
+}
+
+function dichotomy(nums, begin, end) {
+    if (begin > end) {
+        return null;
     }
+    var mid = Math.floor((begin + end) / 2);
+    var root = new TreeNode(nums[mid]);//根结点，为当前数组的中间值
+    root.left = dichotomy(nums, begin, mid - 1);//根结点的左孩子为左半段数组的中间值
+    root.right = dichotomy(nums, mid + 1, end);//根结点的右孩子为右半段数组的中间值
     return root;
 }
