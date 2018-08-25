86518ff601c16c7ab97e5f404d43ac47b3cd3264
carlzz <carlzzzz@163.com>
2018-08-25 22:39:27 +0800

    middle level

---
+++ array/lengthOfLongestSubstring.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+    var arr = s.split('')
+    let start = 0,
+        max = arr.length ? 1 : 0,
+        pos = 1;
+    while (pos <= arr.length ) {
+        let exist = arr.slice(start, pos),
+            gap = exist.length;
+        if (exist.indexOf(arr[pos]) === -1) {
+            pos++;
+        } else {
+            start++;
+        }
+        max = Math.max(max, gap);
+    }
+    return max;
+};
+
+console.log(lengthOfLongestSubstring(''));
