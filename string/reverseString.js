927c26665a9cc7e7c428dbe98e92ed45d83c013f
carlzz <carlzzzz@163.com>
2019-07-01 23:05:20 +0800

    fix

--- string/reverseString.js
+++ string/reverseString.js
@@ -1,7 +1,16 @@
 /**
- * @param {string} s
- * @return {string}
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
  */
-var reverseString = function(s) {
-    return s.split('').reverse().join('')
+var reverseString = function (s) {
+    var start = 0, end = s.length - 1;
+
+    while (start < end) {
+        var temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        start++;
+        end--;
+    }
+    return s;
 };
