ced57c40f7cdc82c7f5fbc727bf6eef9a0bdd967
carlzz <carlzzzz@163.com>
2019-07-20 21:34:33 +0800

    done hard

--- hashSet/findDuplicateSubtrees.js
+++ hashSet/findDuplicateSubtrees.js
@@ -10,5 +10,26 @@
  * @return {TreeNode[]}
  */
 var findDuplicateSubtrees = function(root) {
-    
+    var res = [];
+    var map = new Map();
+    if (root == null) return res;
+    saveRoute(root, res, map);
+    return res;
+};
+
+//递归获取每个子树的路径，保存于Map中
+var saveRoute = function (node, res, map) {
+    if (node == null) return '';
+    //自底向上获取每个节点的序列化值
+    var route = node.val + ',' + saveRoute(node.left, res, map) + ',' + saveRoute(
+        node.right,
+        res,
+        map);
+    //将结果放入map，判断是否有相同子树
+    //避免出现多次相同子树
+    if (map.get(route) != null && map.get(route) == 1) {
+        res.add(node);
+    }
+    map.set(route, map.has(route) ? map.get(route) + 1 : 0);
+    return route;
 };
