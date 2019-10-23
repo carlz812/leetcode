214ac0daf4873add244a6ea9623b73c57fdf11ae
carlzz <carlzzzz@163.com>
2019-10-23 22:12:47 +0800

    math

--- tree/lowestCommonAncestor.js
+++ tree/lowestCommonAncestor.js
@@ -17,6 +17,7 @@ var lowestCommonAncestor = function (root, p, q) {
         if (root === null) {
             return false;
         }
+        // 目标节点也有可能是最近的祖先节点， 三选二
         var center = (root.val === p.val || root.val === q.val) ? 1 : 0;
         var left = inner(root.left, p, q) ? 1 : 0;
         var right = inner(root.right, p, q) ? 1 : 0;
