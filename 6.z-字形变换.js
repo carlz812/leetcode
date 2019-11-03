10905b8de5cf4c6d699568c858e39278d2662917
袁程 <carlzzzz@163.com>
2019-11-03 21:32:15 +0800

    2019-11-03

---
+++ 6.z-字形变换.js
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode.cn id=6 lang=javascript
+ *
+ * [6] Z 字形变换
+ *
+ * https://leetcode-cn.com/problems/zigzag-conversion/description/
+ *
+ * algorithms
+ * Medium (45.41%)
+ * Likes:    433
+ * Dislikes: 0
+ * Total Accepted:    69.4K
+ * Total Submissions: 152.6K
+ * Testcase Example:  '"PAYPALISHIRING"\n3'
+ *
+ * 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+ * 
+ * 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+ * 
+ * L   C   I   R
+ * E T O E S I I G
+ * E   D   H   N
+ * 
+ * 
+ * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。
+ * 
+ * 请你实现这个将字符串进行指定行数变换的函数：
+ * 
+ * string convert(string s, int numRows);
+ * 
+ * 示例 1:
+ * 
+ * 输入: s = "LEETCODEISHIRING", numRows = 3
+ * 输出: "LCIRETOESIIGEDHN"
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: s = "LEETCODEISHIRING", numRows = 4
+ * 输出: "LDREOEIIECIHNTSG"
+ * 解释:
+ * 
+ * L     D     R
+ * E   O E   I I
+ * E C   I H   N
+ * T     S     G
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+    if(numRows === 1){
+        return s;
+    }
+    var rows = 0;
+    var isDown = true;
+    var res = Array(numRows).fill('');
+
+    for(var i = 0; i<s.length;i++){
+        var curr = s[i];
+
+        res[rows] += curr;
+
+        if(rows === 0){
+            isDown = true
+        }
+        if(rows === numRows - 1){
+            isDown = false;
+        }
+        if(isDown){
+            rows ++;
+        } else {
+            rows --;
+        }
+    }
+    return res.join('');
+};
