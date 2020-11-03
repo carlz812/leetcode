5f51972f4bb33812a11ed9d42a3dc0fe88520fc8
carlzz <carlzzzz@163.com>
2020-11-03 15:17:48 +0800

    feat(offer): easy

---
+++ 104.二叉树的最大深度.js
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=javascript
+ *
+ * [104] 二叉树的最大深度
+ *
+ * https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (75.08%)
+ * Likes:    729
+ * Dislikes: 0
+ * Total Accepted:    297.8K
+ * Total Submissions: 395.6K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，找出其最大深度。
+ *
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例：
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ *
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ *
+ * 返回它的最大深度 3 。
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  var max = 0;
+
+  var getChild = function (node, count) {
+    if (node.left) {
+      getChild(node.left, count + 1);
+    }
+    if (node.right) {
+      getChild(node.right, count + 1);
+    }
+    max = Math.max(max, count);
+  };
+
+  if (root) {
+    getChild(root, 1);
+  }
+  return max;
+};
+
+// @lc code=end
